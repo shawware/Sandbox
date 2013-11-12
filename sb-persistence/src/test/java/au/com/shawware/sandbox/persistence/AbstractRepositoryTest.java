59fcdfce2c16c9bbcb76e134d05444a21d620401
David Shaw <david.shaw@shawware.com.au>
2013-11-12 20:11:16 +1100

    Refactor common part of node repository (persistence) tests.

---
+++ sb-persistence/src/test/java/au/com/shawware/sandbox/persistence/AbstractRepositoryTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2013 shawware.com.au
+ *
+ * License: GNU General Public License V3 (or later)
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+package au.com.shawware.sandbox.persistence;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Assert;
+
+import au.com.shawware.sandbox.model.Node;
+import au.com.shawware.sandbox.model.NodeType;
+
+/**
+ * Base class for unit tests.
+ *
+ * @author <a href="mailto:david.shaw@shawware.com.au">David Shaw</a>
+ */
+public abstract class AbstractRepositoryTest
+{
+    /** The logger to use for all output. */
+    private Log mLog;
+
+    /**
+     * 
+     */
+    public AbstractRepositoryTest()
+    {
+        super();
+        mLog = LogFactory.getLog(AbstractRepositoryTest.class.getName());
+    }
+
+    /**
+     * Basic tests on on the given repository.
+     * 
+     * @param repo the repository to use
+     */
+    @SuppressWarnings({ "nls" })
+    public void testRepository(final NodeRepository repo)
+    {
+        final Node n = repo.save(new Node(NodeType.Country));
+        mLog.info("First node: " + n);
+        Assert.assertEquals(NodeType.Country, n.getType());
+        Assert.assertEquals(Integer.valueOf(1), n.getId());
+        List<Node> nodes;
+        nodes = repo.findByType(NodeType.Local);
+        Assert.assertEquals("found a local node", 0, nodes.size());
+        nodes = repo.findByType(NodeType.Country);
+        Assert.assertEquals("did not find a country node", 1, nodes.size());
+        final Node n2 = repo.findOne(n.getId());
+        mLog.info("node lookup: " + n2);
+    }
+}
--- sb-persistence/src/test/java/au/com/shawware/sandbox/persistence/JPAJavaConfigTest.java
+++ sb-persistence/src/test/java/au/com/shawware/sandbox/persistence/JPAJavaConfigTest.java
@@ -7,12 +7,8 @@
 
 package au.com.shawware.sandbox.persistence;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -25,9 +21,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-import au.com.shawware.sandbox.model.Node;
-import au.com.shawware.sandbox.model.NodeType;
-
 /**
  * Test our JPA configuration.
  *
@@ -37,10 +30,8 @@ import au.com.shawware.sandbox.model.NodeType;
 @ContextConfiguration(classes = {JPAConfiguration.class})
 @Transactional
 @TransactionConfiguration(defaultRollback = true)
-public class JPAJavaConfigTest
+public class JPAJavaConfigTest extends AbstractRepositoryTest
 {
-    /** The logger to use for all output. */
-    private static Log sLog;
     /** The application context to use for all beans. */
     private static ApplicationContext sContext;
     /** The node repository to use for all transactions. */
@@ -55,7 +46,6 @@ public class JPAJavaConfigTest
     @BeforeClass
     public static void setUp()
     {
-        sLog = LogFactory.getLog(JPAXMLCfgTest.class.getName());
         sContext = new AnnotationConfigApplicationContext(JPAConfiguration.class);
         sRepo = sContext.getBean(NodeRepository.class);
     }
@@ -73,19 +63,8 @@ public class JPAJavaConfigTest
      * Basic tests on our repository.
      */
     @Test
-    @SuppressWarnings("static-method")
     public void testRepository()
     {
-        final Node n = sRepo.save(new Node(NodeType.Country));
-        sLog.info("First node: " + n); //$NON-NLS-1$
-        Assert.assertEquals(NodeType.Country, n.getType());
-        Assert.assertEquals(Integer.valueOf(1), n.getId());
-        List<Node> nodes;
-        nodes = sRepo.findByType(NodeType.Local);
-        Assert.assertEquals("found a local node", 0, nodes.size()); //$NON-NLS-1$
-        nodes = sRepo.findByType(NodeType.Country);
-        Assert.assertEquals("did not find a country node", 1, nodes.size()); //$NON-NLS-1$
-        final Node n2 = sRepo.findOne(n.getId());
-        sLog.info("node lookup: " + n2); //$NON-NLS-1$
+        testRepository(sRepo);
     }
 }
